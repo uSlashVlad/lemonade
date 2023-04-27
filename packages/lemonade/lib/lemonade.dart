fbce8ea19a784b68e05f6eb980d0d05391f3d3c5
Vladislav Nagorny <u.slash.vlad@gmail.com>
2023-04-28 00:01:13 +0300

    Added some comments on public API, fixed examples

--- packages/lemonade/lib/lemonade.dart
+++ packages/lemonade/lib/lemonade.dart
@@ -1,1 +1,5 @@
+/// Base library for universal lemonade validators.
+library lemonade;
+
 export 'package:lemonade/src/validators/validators.dart';
+export 'src/errors.dart';
--- packages/lemonade/lib/src/validators/validators.dart
+++ packages/lemonade/lib/src/validators/validators.dart
@@ -8,15 +8,42 @@ export 'collections_validators.dart' show CollectionValidator;
 export 'compound_validators.dart' show CompoundValidator;
 export 'value_validators.dart' show ValueValidator;
 
+/// Base class for all validators.
+/// Also provides constructors for all basics validators.
+///
+/// You can identify a validator by its [annotation].
 abstract class Validator {
+  /// Main constructor for [Validator].
+  /// Needed to provide [annotation] by subclasses.
   const Validator({required this.annotation});
 
+  /// Matches anything.
+  /// Doesn't mind nulls.
   const factory Validator.any() = AnyValidator;
 
+  /// Matches only null values.
+  /// Almost equals to `Validator.equals(null)`.
   const factory Validator.nullValue() = NullValidator;
 
+  /// Matches only values that equals to [matcher].
+  /// With usage of [Validator.or] can be used to match enum values.
   const factory Validator.equals(Object? matcher) = EqualsValidator;
 
+  /// Matches only numbers (both double and integers).
+  ///
+  /// [min] - minimal number value.
+  /// Can be both integer and double.
+  /// Anything that less than it, will not pass validation.
+  ///
+  /// [max] - maximum number value.
+  /// Can be both integer and double.
+  /// Anything that more than it, will not pass validation.
+  ///
+  /// If you want to match to only specific number by passing same [min] and
+  /// [max] values, then you should use [Validator.equals].
+  ///
+  /// If you want to match only integer values, you should use
+  /// [Validator.integer].
   factory Validator.number({
     num? min,
     num? max,
@@ -26,6 +53,19 @@ abstract class Validator {
         max: max,
       );
 
+  /// Matches only integer numbers.
+  ///
+  /// [min] - minimal number value.
+  /// Anything that less than it, will not pass validation.
+  ///
+  /// [max] - maximum number value.
+  /// Anything that more than it, will not pass validation.
+  ///
+  /// If you want to match to only specific number by passing same [min] and
+  /// [max] values, then you should use [Validator.equals].
+  ///
+  /// If you want to match both integer and double values, you should use
+  /// [Validator.number].
   factory Validator.integer({
     int? min,
     int? max,
@@ -36,12 +76,48 @@ abstract class Validator {
         integer: true,
       );
 
+  /// Matches only strings.
+  ///
+  /// [minLength] - minimal string length.
+  /// Anything that shorter than it, will not pass validation.
+  ///
+  /// [maxLength] - maximum string length.
+  /// Anything that longer than it, will not pass validation.
+  ///
+  /// So if you want to match only specific length, you should specify same
+  /// [minLength] and [maxLength].
+  /// [Validator.equals] won't do the trick for strings.
+  ///
+  /// [pattern] - string or regex pattern.
+  /// If input string has not matched the pattern, will not pass validation.
+  /// So if [String] is passed, it will pass if there is at least one occurrence
+  /// of [pattern].
+  /// If [RegExp] is passed, it will pass if there is at least one regexp match
+  /// in input string.
   factory Validator.string({
     int? maxLength,
     int? minLength,
     Pattern? pattern,
   }) = StringValidator;
 
+  /// Matches only [Iterable].
+  /// Does not do any assumptions about [Iterable] subclasses.
+  /// So it works for [List], [Set], [Queue], [LinkedList] and other classes.
+  ///
+  /// [item] - validator for each item in input list.
+  /// [Validator.any] by default.
+  ///
+  /// [minItems] - minimum number of items.
+  /// Any list that shorter than it, will not pass validation.
+  ///
+  /// [maxItems] - maximum number of items.
+  /// Any list that longer than it, will not pass validation.
+  ///
+  /// So if you want to match only specific number of items, you should specify
+  /// same [minItems] and [maxItems].
+  ///
+  /// [uniqueItems] - uniqueness of list items.
+  /// If true, then each item must be unique.
   factory Validator.list({
     Validator item,
     int? maxItems,
@@ -49,6 +125,26 @@ abstract class Validator {
     bool uniqueItems,
   }) = IterableValidator;
 
+  /// Matches only [Map].
+  /// Works for maps that are returned from [jsonDecode] function.
+  ///
+  /// [key] - validator for each key in map.
+  /// [Validator.any] by default.
+  ///
+  /// [value] - validator for each value in map.
+  /// [Validator.any] by default.
+  ///
+  /// Firstly tries to match [key], then [value].
+  /// Iterates through map by calling [Map.entries].
+  ///
+  /// [minItems] - minimum number of items.
+  /// Any map that shorter than it, will not pass validation.
+  ///
+  /// [maxItems] - maximum number of items.
+  /// Any map that longer than it, will not pass validation.
+  ///
+  /// So if you want to match only specific number of items, you should specify
+  /// same [minItems] and [maxItems].
   factory Validator.map({
     Validator key,
     Validator value,
@@ -56,13 +152,30 @@ abstract class Validator {
     int? minItems,
   }) = MapValidator;
 
+  /// Matches only [Map].
+  /// Works for maps that are returned from [jsonDecode] function.
+  ///
+  /// [items] - fields from input map that have to be checked.
+  /// It does not check any fields that are not present in [items].
+  ///
+  /// [ignoreExtra] - if false, will not pass if there is any excess fields in
+  /// input map.
+  /// true by default.
   factory Validator.object({
     Map<String, Validator> items,
     bool ignoreExtra,
   }) = ObjectValidator;
 
+  /// Matches if any of [children] passes validation.
+  /// Similar to "||" operator in boolean expressions.
+  ///
+  /// Validates in the same order in which they are specified.
   factory Validator.or(List<Validator> children) = OrValidator;
 
+  /// Matches if all of [children] passes validation.
+  /// Similar to "&&" operator in boolean expressions.
+  ///
+  /// Validates in the same order in which they are specified.
   factory Validator.and(List<Validator> children) = AndValidator;
 
   final String annotation;
@@ -73,12 +186,27 @@ abstract class Validator {
 
   ValidationError? getError(dynamic data);
 
+  /// Matches if either validator or [Validator.nullValue] passes validation.
+  ///
+  /// In general should be used on any non-nullable validator.
+  /// But can be used even on [Validator.nullValue] (which is absolutely
+  /// useless).
   Validator nullable() => OrValidator([this, const NullValidator()]);
 
+  /// Matches if either this or [other] passes validation.
+  /// Similar to "||" operator in boolean expressions.
+  ///
+  /// Returns [Validator.or] on 2 validators.
+  /// Firstly validates this, then [other].
   Validator operator |(Validator other) {
     return OrValidator([this, other]);
   }
 
+  /// Matches if both this and [other] passes validation.
+  /// Similar to "&&" operator in boolean expressions.
+  ///
+  /// Returns [Validator.and] on 2 validators.
+  /// Firstly validates this, then [other].
   Validator operator &(Validator other) {
     return AndValidator([this, other]);
   }
