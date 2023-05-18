6191ad41ebd55c489620cd8dbfa80ba44adda78c
Vladislav Nagorny <u.slash.vlad@gmail.com>
2023-05-18 18:24:50 +0300

    Added tests on some common patterns, fixed email and base64 validators

--- packages/lemonade/lib/src/patterns/validators.dart
+++ packages/lemonade/lib/src/patterns/validators.dart
@@ -52,7 +52,7 @@ abstract class Validators {
   /// This may not conform to all possibilities of any rfc standard, but neither
   /// does any email provider accept all possibilities.
   static Validator email() => _validatorForRegex(
-        r"^(?:(?:(?:(?:[a-zA-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])+(?:\.([a-zA-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])+)*)|(?:(?:\x22)(?:(?:(?:(?:\x20|\x09)*(?:\x0d\x0a))?(?:\x20|\x09)+)?(?:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])|(?:(?:[\x01-\x09\x0b\x0c\x0d-\x7f]|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}]))))*(?:(?:(?:\x20|\x09)*(?:\x0d\x0a))?(\x20|\x09)+)?(?:\x22))))@(?:(?:(?:[a-zA-Z]|\d|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])|(?:(?:[a-zA-Z]|\d|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])(?:[a-zA-Z]|\d|-|\.|~|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])*(?:[a-zA-Z]|\d|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])))\.)+(?:(?:[a-zA-Z]|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])|(?:(?:[a-zA-Z]|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])(?:[a-zA-Z]|\d|-|\.|~|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])*(?:[a-zA-Z]|[\x{00A0}-\x{D7FF}\x{F900}-\x{FDCF}\x{FDF0}-\x{FFEF}])))\.?$",
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
       );
 
   // TODO(uSlashVlad): Add "File path" validator.
@@ -63,7 +63,7 @@ abstract class Validators {
 
   /// This validates that a string value contains a valid base64 value.
   static Validator base64() => _validatorForRegex(
-        r'^(?:[A-Za-z0-9+\\/]{4})*(?:[A-Za-z0-9+\\/]{2}==|[A-Za-z0-9+\\/]{3}=|[A-Za-z0-9+\\/]{4})$',
+        r'^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{4})$',
         caseSensitive: true,
       );
 
---
+++ packages/lemonade/test/patterns/validators_test.dart
@@ -0,0 +1,123 @@
+import 'package:lemonade/src/patterns/validators.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Validators.alpha()', () {
+    test('alpha() returns true for valid input', () {
+      final validator = Validators.alpha();
+      expect(validator.validate('HelloWorld'), true);
+      expect(validator.validate('abcdefghijklmnopqrstuvwxyz'), true);
+    });
+
+    test('alpha() returns false for invalid input', () {
+      final validator = Validators.alpha();
+      expect(validator.validate('Hello World'), false);
+      expect(validator.validate('Hello123'), false);
+    });
+  });
+
+  group('Validators.alphanumeric()', () {
+    test('alphanumeric() returns true for valid input', () {
+      final validator = Validators.alphanumeric();
+      expect(validator.validate('Example123'), true);
+      expect(validator.validate('abcdefghijklmnopqrstuvwxyz0123456789'), true);
+    });
+
+    test('alphanumeric() returns false for invalid input', () {
+      final validator = Validators.alphanumeric();
+      expect(validator.validate('Example 123'), false);
+      expect(validator.validate('Example!'), false);
+    });
+  });
+
+  group('Validators.numeric()', () {
+    test('numeric() returns true for valid input', () {
+      final validator = Validators.numeric();
+      expect(validator.validate('123'), true);
+      expect(validator.validate('-123'), true);
+      expect(validator.validate('3.14'), true);
+      expect(validator.validate('-3.14'), true);
+    });
+
+    test('numeric() returns false for invalid input', () {
+      final validator = Validators.numeric();
+      expect(validator.validate('123a'), false);
+      expect(validator.validate('12.3.4'), false);
+    });
+  });
+
+  group('Validators.hexadecimal()', () {
+    test('hexadecimal() returns true for valid input', () {
+      final validator = Validators.hexadecimal();
+      expect(validator.validate('c702f6f5'), true);
+      expect(validator.validate('abcdef0123456789'), true);
+    });
+
+    test('hexadecimal() returns false for invalid input', () {
+      final validator = Validators.hexadecimal();
+      expect(validator.validate('c702g6f5'), false);
+      expect(validator.validate('hellothere'), false);
+    });
+  });
+
+  group('Validators.hexcolor()', () {
+    test('hexcolor() returns true for valid input', () {
+      final validator = Validators.hexcolor();
+      expect(validator.validate('#eb0c0c'), true);
+      expect(validator.validate('#EEE'), true);
+      expect(validator.validate('#abc123'), true);
+    });
+
+    test('hexcolor() returns false for invalid input', () {
+      final validator = Validators.hexcolor();
+      expect(validator.validate('#eb0c0'), false);
+      expect(validator.validate('eb0c0c'), false);
+      expect(validator.validate('#hellothere'), false);
+    });
+  });
+
+  group('Validators.email()', () {
+    test('email() returns true for valid input', () {
+      expect(Validators.email().validate('test@example.com'), true);
+      expect(Validators.email().validate('test+filter@example.com'), true);
+      expect(Validators.email().validate('test-123@example.com'), true);
+      expect(Validators.email().validate('test.123@example.com'), true);
+      expect(Validators.email().validate('test@example.co.uk'), true);
+      expect(Validators.email().validate('test@example.io'), true);
+    });
+
+    test('email() returns false for invalid input', () {
+      expect(Validators.email().validate('invalid_email'), false);
+      expect(Validators.email().validate('test@example'), false);
+      expect(Validators.email().validate('test.@example.com'), false);
+      expect(Validators.email().validate('test@example..com'), false);
+      expect(Validators.email().validate('test@.example.com'), false);
+      expect(Validators.email().validate('.test@example.com'), false);
+      expect(Validators.email().validate('test@example.c'), false);
+      expect(Validators.email().validate('test@example.'), false);
+    });
+  });
+
+  group('Validators.base64()', () {
+    test('base64() returns true for valid input', () {
+      expect(Validators.base64().validate('dGVzdA=='), true);
+      expect(Validators.base64().validate('dGVzdA'), true);
+      expect(Validators.base64().validate('dGVzdA+'), true);
+      expect(Validators.base64().validate('dGVzdA+/'), true);
+      expect(Validators.base64().validate('/9j/4AAQSkZJRgABAgEBLAEsAAD/'), true);
+      expect(Validators.base64().validate('c29tZSBkYXRhIHdpdGggACBhbmQg77u/'), true);
+    });
+
+    test('base64() returns false for invalid input', () {
+      expect(Validators.base64().validate('dGVzdA='), false);
+      expect(Validators.base64().validate('dGVzdA+=='), false);
+      expect(Validators.base64().validate('dGVzdA+/=='), false);
+      expect(Validators.base64().validate('dGVzdA+/'), false);
+      expect(Validators.base64().validate('dGVzdA+'), false);
+      expect(Validators.base64().validate('dGVzdA'), false);
+      expect(Validators.base64().validate('test data'), false);
+      expect(Validators.base64().validate(r'!@#$'), false);
+    });
+  });
+
+}
