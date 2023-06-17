cc4f981b232afb8c7bac22aac3e0cf3f35b761b0
Vladislav Nagorny <u.slash.vlad@gmail.com>
2023-06-17 23:09:27 +0300

    Added tests for some common patterns

--- packages/lemonade/test/patterns/validators_test.dart
+++ packages/lemonade/test/patterns/validators_test.dart
@@ -122,13 +122,13 @@ void main() {
   });
 
   group('Validators.base64Url()', () {
-    test('returns true for valid input', () {
+    test('base64Url() returns true for valid input', () {
       final validator = Validators.base64Url();
       expect(validator.validate('aGVsbG8='), true);
       expect(validator.validate('aGVsbG8gd29ybGQ='), true);
     });
 
-    test('returns false for invalid input', () {
+    test('base64Url() returns false for invalid input', () {
       final validator = Validators.base64Url();
       expect(validator.validate('aGVsbG8!*'), false);
       expect(validator.validate('aGVsbG8gd29ybGQ?'), false);
@@ -137,14 +137,14 @@ void main() {
   });
 
   group('Validators.btcAddress()', () {
-    test('returns true for valid input', () {
+    test('btcAddress() returns true for valid input', () {
       final validator = Validators.btcAddress();
       expect(validator.validate('1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2'), true);
       expect(validator.validate('3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy'), true);
       expect(validator.validate('1AGNa15ZQXAZUgFiqJ2i7Z2DPU2J6hW62i'), true);
     });
 
-    test('returns false for invalid input', () {
+    test('btcAddress() returns false for invalid input', () {
       final validator = Validators.btcAddress();
       expect(validator.validate('Hello World'), false);
       expect(validator.validate('1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN!'), false);
@@ -154,7 +154,7 @@ void main() {
   });
 
   group('Validators.btcAddressBech32()', () {
-    test('returns true for valid input', () {
+    test('btcAddressBech32() returns true for valid input', () {
       final validator = Validators.btcAddressBech32();
       expect(
         validator.validate('bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4'),
@@ -163,7 +163,7 @@ void main() {
       expect(validator.validate('bc1zw508d6qejxtdg4y5r3zarvaryvaxxpcs'), true);
     });
 
-    test('returns false for invalid input', () {
+    test('btcAddressBech32() returns false for invalid input', () {
       final validator = Validators.btcAddressBech32();
       expect(validator.validate('Hello World'), false);
       expect(validator.validate('1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2'), false);
@@ -172,7 +172,7 @@ void main() {
   });
 
   group('Validators.ethAddress()', () {
-    test('returns true for valid input', () {
+    test('ethAddress() returns true for valid input', () {
       final validator = Validators.ethAddress();
       expect(
         validator.validate('0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed'),
@@ -184,7 +184,7 @@ void main() {
       );
     });
 
-    test('returns false for invalid input', () {
+    test('ethAddress() returns false for invalid input', () {
       final validator = Validators.ethAddress();
       expect(validator.validate('Hello World'), false);
       expect(
@@ -199,16 +199,65 @@ void main() {
   });
 
   group('Validators.uuid()', () {
-    test('returns true for valid input', () {
+    test('uuid() returns true for valid input', () {
       final validator = Validators.uuid();
-      expect(validator.validate('67fcbcdf-78a3-4c63-8a3e-7abf7c6a12f9'), true);
+      expect(validator.validate('67fcbcdf-78a3-0c63-8a3e-7abf7c6a12f9'), true);
+      expect(validator.validate('519564ab-ef9f-31b3-83dd-c3c798f0d42c'), true);
       expect(validator.validate('f8e2bfc6-6d2e-4a3e-9f85-75d2f0bb2c04'), true);
+      expect(validator.validate('df3b4230-8473-59ee-956e-361ee161ce89'), true);
     });
 
-    test('returns false for invalid input', () {
+    test('uuid() returns false for invalid input', () {
       final validator = Validators.uuid();
       expect(validator.validate('67fcbcdf-78a3-4c63-8a3e-7abf7c6a12fg'), false);
       expect(validator.validate('67FCBCDF-78A3-4C63-8A3E-7ABF7C6A12F9'), false);
+      expect(validator.validate('67fcbcdf-78a3-4c63-8a3e-7abf7c6a12f'), false);
+    });
+  });
+
+  group('Validators.uuid3()', () {
+    test('uuid3() returns true for valid input', () {
+      final validator = Validators.uuid3();
+      expect(validator.validate('519564ab-ef9f-31b3-83dd-c3c798f0d42c'), true);
+      expect(validator.validate('46cb1863-1df8-322b-87f1-413236a098ad'), true);
+    });
+
+    test('uuid3() returns false for invalid input', () {
+      final validator = Validators.uuid3();
+      expect(validator.validate('67fcbcdf-78a3-0c63-8a3e-7abf7c6a12f9'), false);
+      expect(validator.validate('f8e2bfc6-6d2e-4a3e-9f85-75d2f0bb2c04'), false);
+      expect(validator.validate('df3b4230-8473-59ee-956e-361ee161ce89'), false);
+      expect(validator.validate('67FCBCDF-78A3-4C63-8A3E-7ABF7C6A12F9'), false);
+      expect(validator.validate('67fcbcdf-78a3-4c63-8a3e-7abf7c6a12f'), false);
+    });
+  });
+
+  group('Validators.uuid4()', () {
+    test('uuid4() returns true for valid input', () {
+      final validator = Validators.uuid4();
+      expect(validator.validate('6ba7b810-9dad-41d1-80b4-00c04fd430c8'), true);
+      expect(validator.validate('9d5b0b1a-96c9-49cc-8765-a37af23b2ddc'), true);
+    });
+
+    test('uuid4() returns false for invalid input', () {
+      final validator = Validators.uuid4();
+      expect(validator.validate('519564ab-ef9f-31b3-83dd-c3c798f0d42c'), false);
+      expect(validator.validate('e94464a7-cffc-5f6b-ac38-c3c849fa8ea1'), false);
+      expect(validator.validate('67fcbcdf-78a3-4c63-8a3e-7abf7c6a12f'), false);
+    });
+  });
+
+  group('Validators.uuid5()', () {
+    test('uuid5() returns true for valid input', () {
+      final validator = Validators.uuid5();
+      expect(validator.validate('6ba7b810-9dad-51d1-80b4-00c04fd430c8'), true);
+      expect(validator.validate('9d5b0b1a-96c9-59cc-8765-a37af23b2ddc'), true);
+    });
+
+    test('uuid5() returns false for invalid input', () {
+      final validator = Validators.uuid5();
+      expect(validator.validate('519564ab-ef9f-31b3-83dd-c3c798f0d42c'), false);
+      expect(validator.validate('e94464a7-cffc-4f6b-ac38-c3c849fa8ea1'), false);
       expect(validator.validate('67fcbcdf-78a3-4c63-8a3e-7abf7c6a12f'), false);
     });
   });
